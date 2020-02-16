b9145682a0721dfb4d3dd8de08d630cf55fa4ceb
paavo <shibaev.pp@phystech.edu>
2020-02-16 06:22:01 -0500

    Seems to be ready for demonstration

--- main.c
+++ main.c
@@ -1,32 +1,45 @@
-/*
+/**
+ * @brief
  * compare sorts on random arrays
  * Sorting methods implemented:
  *  - simple insertion sort
  *  - recursive QSort
  */
 
+// UNCOMMENT TO GET SPECIFIC MODES INCLUDED IN THE BUILD
+#define RUN_WITH_PRINT
+// #define RUN_WITH_STATS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
 #include <assert.h>
+#include <time.h>
 
-/*
- * Generate random double
- */
 enum {
     BOUND = 10000
 };
+
+/**
+ * @brief
+ * Generate random double
+ */
 double
 rand_double (void)
 {
+  // return ((double) rand()) / RAND_MAX;
+  // return (double) BOUND + rand () / ((double) RAND_MAX / (2 * BOUND));
   double r = rand () % BOUND;
+  r *= (rand () % 2) ? -1 : 1;
   double q = (double) 1 / (rand () % BOUND);
   // return some pseudorandom int + some pseudorandom fraction
   return r + q;
 }
 
-/*
+
+/**
+ * @brief
  * Generates a sorted array of 0.0, 1.0, 2.0, ... n - 1
  */
 double *
@@ -40,7 +53,8 @@ generate_sorted (size_t n)
   return arr;
 }
 
-/*
+/**
+ * @brief
  * Generates a reverse sorted array of n - 1, n - 2, ..., 0.0
  */
 double *
@@ -54,7 +68,8 @@ generate_reverse (size_t n)
   return arr;
 }
 
-/*
+/**
+ * @brief
  * Generates random array of size t
  */
 double *
@@ -68,7 +83,8 @@ generate_random (size_t n)
   return arr;
 }
 
-/*
+/**
+ * @brief
  * Allocates memory and copies the src array to it.
  * Returns array pointer
  */
@@ -93,7 +109,8 @@ print_arr (double *arr, size_t n)
   printf ("\n");
 }
 
-/*
+/**
+ * @brief
  * Returns true if absolute value does NOT increase
  */
 bool
@@ -102,7 +119,7 @@ cmp (double other, double another)
   return fabs (other) >= fabs (another);
 }
 
-/*
+/**
  * Returns true, if doubles are really close
  */
 const double EPS = 0.00000005;
@@ -112,7 +129,7 @@ double max (double a, double b)
 }
 
 
-/*
+/** @brief
  * Relative comparison of two duobles
  */
 bool
@@ -127,7 +144,7 @@ equals (double other, double another)
 }
 
 
-/*
+/** @brief
  * Simple check for trivial case when src is sorted
  */
 bool
@@ -141,7 +158,7 @@ check_trivial (const double *src, const double *sorted, size_t n)
   return true;
 }
 
-/*
+/** @brief
  * Just the same but for the reversed src case
  */
 bool
@@ -156,36 +173,40 @@ check_reverse (const double *src, const double *sorted, size_t n)
 }
 
 bool
-permucheck(double *src, double *sorted, size_t n)
+permucheck (double *src, double *sorted, size_t n)
 {
-  for (size_t i = 0; i < n; ++i)
+  bool *marks = malloc (n * sizeof (bool));
+  for (size_t j = 0; j < n; ++j)
     {
-      int skip = false;
-      for (size_t j = 0; j < n; ++j)
+      for (size_t i = 0; i < n; ++i)
         {
-          if (equals (src[i], sorted[j]))
+          if (equals (src[i], sorted[j]) && !marks[j])
             {
-              skip = 1;
+              marks[j] = 1;
               break;
             }
         }
-      if (!skip)
+    }
+  for (size_t i = 0; i < n; ++i)
+    {
+      if (!marks[i])
         return false;
     }
+  free (marks);
   return true;
 }
 
 bool
-is_sorted(double *sorted, size_t n)
+is_sorted (double *sorted, size_t n)
 {
   for (size_t i = 1; i < n; ++i)
     {
-      if (!cmp(sorted[i - 1], sorted[i]))
+      if (!cmp (sorted[i - 1], sorted[i]))
         return false;
     }
   return true;
 }
-/*
+/** @brief
  * Assures whether the src array is the permutation of sorted's elements && and sorted is actually sorted
  */
 bool
@@ -196,7 +217,7 @@ check_correct (double *src, double *sorted, size_t n)
 
 // HELPER FUNCTIONS FOR SORTING
 size_t gNumSwaps = 0;
-/*
+/** @brief
  * Swap values and increase counter
  */
 void
@@ -208,7 +229,7 @@ swap_with_count (double *other, double *another)
   ++gNumSwaps;
 }
 
-/*
+/** @brief
  *  Compare two numbers and increase counter
  */
 // total number of comparisons
@@ -229,20 +250,23 @@ avg (const int arr[], int n)
   return res / n;
 }
 
-/*
+/** @brief
  * print stats for specific case
  */
+enum {
+    NCASES = 4
+};
 void
 print_case (int size, int swaps[], int cmps[])
 {
   printf ("Printing stats for size %d\n", size);
-  printf ("Number of swaps %d, %d, %d, %d ---> %f\n", swaps[0], swaps[1],
-          swaps[2], swaps[3], avg (swaps, 4));
-  printf ("Number of comparisons %d, %d, %d, %d ---> %f\n", cmps[0], cmps[1],
-          cmps[2], cmps[3], avg (cmps, 4));
+  printf ("Number of swaps %d & %d & %d & %d & %f\n", swaps[0], swaps[1],
+          swaps[2], swaps[3], avg (swaps, NCASES));
+  printf ("Number of comparisons %d & %d & %d & %d & %f\n", cmps[0], cmps[1],
+          cmps[2], cmps[3], avg (cmps, NCASES));
 }
 
-/*
+/** @brief
  * Sets global parameters to zero
  */
 void
@@ -251,21 +275,21 @@ zero (void)
   gNumSwaps = gNumCmp = 0;
 }
 
-/*
+/** @brief
  * Handles all the assessment routine
  */
 // sizes of arrays to be tested
-const int kRanges[] = {10, 100, 1000, 10000};
+const int kRanges[] = {10, 100, 1000, 10000, 100000};
 void
-driver (void (*method) (double*, size_t,
+driver (void (*method) (double *, size_t,
                         bool (*) (double, double),
                         void (*) (double *, double *)
 ))
 {
-  int cmps[4];
-  int swaps[4];
+  int cmps[NCASES];
+  int swaps[NCASES];
 
-  for (int i = 0; i < 4; ++i)
+  for (int i = 0; i < NCASES; ++i)
     {
       int sz = kRanges[i];
 
@@ -317,7 +341,7 @@ driver (void (*method) (double*, size_t,
     }
 }
 
-/*
+/** @brief
  * Simple selection sort
  */
 void
@@ -331,7 +355,7 @@ selection (double *arr, size_t sz,
       size_t min = i;
       for (size_t j = i + 1; j < sz; ++j)
         {
-          if (cmp(arr[j], arr[min]))
+          if (cmp (arr[j], arr[min]))
             min = j;
         }
       if (min != i)
@@ -341,56 +365,88 @@ selection (double *arr, size_t sz,
     }
 }
 
-/*
+/** @brief
  * Partition for the quicksort
  */
 size_t
-partition(double* arr, size_t l, size_t r, bool (*cmp)(double, double), void (*swap)(double*, double*))
+partition (double *arr, size_t l, size_t r, bool (*cmp) (double, double), void (*swap) (double *, double *))
 {
-  size_t pivot_i = rand() % (r - l + 1);
-  swap_with_count (arr + r, arr + pivot_i);
   size_t i = l;
-  double pivot = arr[r];
-  for (size_t j = l; j <= r; ++j)
+  size_t j = r + 1;
+
+  double v = arr[i];
+  for (;;)
     {
-      if (!cmp(arr[i], pivot)) {
-        swap(arr + i, arr + j);
-        ++i;
-      }
+      while (cmp (arr[++i], v))
+        {
+          if (i == r)
+            break;
+        }
+      while (cmp (v, arr[--j]))
+        {
+          if (j == l)
+            break;
+        }
+      if (i >= j)
+        break;
+      swap (arr + i, arr + j);
     }
-  swap(arr + i, arr + r);
-  return i;
+  swap (arr + l, arr + j);
+  return j;
 }
 
 void
-quick_sort_r(double* arr, size_t l, size_t r, bool (*cmp)(double, double), void (*swap)(double*, double*))
+quick_sort_r (double *arr, int l, int r, bool (*cmp) (double, double), void (*swap) (double *, double *))
 {
   if (l < r)
     {
-      size_t p = partition (arr, l, r, cmp, swap);
+      int p = partition (arr, l, r, cmp, swap);
       quick_sort_r (arr, l, p - 1, cmp, swap);
       quick_sort_r (arr, p + 1, r, cmp, swap);
     }
 }
 
-void quick_sort(double *arr, size_t sz,
-                bool (*cmp) (double, double),
-                void (*swap) (double *, double *)
+void quick_sort (double *arr, size_t sz,
+                 bool (*cmp) (double, double),
+                 void (*swap) (double *, double *)
 )
 {
   quick_sort_r (arr, 0, sz - 1, cmp, swap);
 }
 
+void
+run_with_print (void)
+{
+  double *arr = generate_random (kRanges[0]);
+  print_arr (arr, kRanges[0]);
+  selection (arr, kRanges[0], cmp, swap_with_count);
+  print_arr (arr, kRanges[0]);
+  free (arr);
+
+  arr = generate_random (kRanges[0]);
+  print_arr (arr, kRanges[0]);
+  quick_sort (arr, kRanges[0], cmp, swap_with_count);
+  print_arr (arr, kRanges[0]);
+  free (arr);
+}
+
 int
 main (void)
 {
-  srand (1);
-  printf("Performing runs for SELECTION SORT\n");
-  driver(selection);
-  printf("COMPLETE!\n");
+  srand(time(0));
+#ifdef RUN_WITH_PRINT
+  run_with_print ();
+#endif
 
-  printf("Performing runs for RECURSIVE QSORT\n");
-  driver(quick_sort);
-  printf("COMPLETE!\n");
+#ifdef RUN_WITH_STATS
+  printf ("Performing runs for SELECTION SORT\n");
+  driver (selection);
+  printf ("COMPLETE!\n");
+
+  printf ("Performing runs for RECURSIVE QSORT\n");
+  driver (quick_sort);
+  printf ("COMPLETE!\n");
+#endif
+
   return 0;
 }
